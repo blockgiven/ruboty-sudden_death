a2a1bad4742652db912e6976e0202b6c5410bd06
block_given? <block_given@outlook.com>
2014-10-18 12:31:44 +0900

    は〜突然の仕様変更〜などにも反応するようにした

--- lib/ruboty/handlers/sudden_death.rb
+++ lib/ruboty/handlers/sudden_death.rb
@@ -1,7 +1,7 @@
 module Ruboty
   module Handlers
     class SuddenDeath < Base
-      on /(?<sudden_death>突然の.*)/, name: 'sudden_death', description: '突然のxx', all: true, hidden: true
+      on /.*(?<sudden_death>突然の.*)/, name: 'sudden_death', description: '突然のxx', all: true, hidden: true
 
       def sudden_death(message)
         message.reply("#{$/}#{message[:sudden_death].sudden_death}#{$/}")
